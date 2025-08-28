b99177371e091288fc5e499d368c4b850232c990
leandroyyy <leandro.cavallari123@gmail.com>
2025-08-27 22:56:54 -0300

    feat: created workout form

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,20 @@
 import { useState } from "react";
 import { Header } from "./components/header";
+import { WorkoutForm } from "./components/workout-form";
 import { WorkoutList } from "./components/workout-list";
 import type { Workout } from "./types/workout";
 
 function App() {
-  const [list, setList] = useState<Workout[]>([
-    {
-      id: "1",
-      title: "Aula de zumba",
-      date: "20/06/2026",
-      duration: 50,
-      intensity: 1,
-    },
-    {
-      id: "2",
-      title: "Aula de fitdance",
-      date: "21/06/2026",
-      duration: 45,
-      intensity: 2,
-    },
-  ]);
+  const [list, setList] = useState<Workout[]>([]);
+
+  function addWorkout(workout: Workout): void {
+    setList((prev) => [...prev, workout]);
+  }
 
   return (
     <>
       <Header />
-      <button
-        onClick={() =>
-          setList([
-            ...list,
-            {
-              id: "3",
-              date: "20/08/2026",
-              duration: 60,
-              intensity: 5,
-              title: "jiu-jitsu",
-            },
-          ])
-        }
-      >
-        adicionar aula
-      </button>
+      <WorkoutForm onAdd={addWorkout} />
       <WorkoutList workoutList={list} />
     </>
   );
