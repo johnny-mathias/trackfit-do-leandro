862de06c08132192ccfbbf86522d6d2ba8bafef2
leandroyyy <leandro.cavallari123@gmail.com>
2025-09-09 20:57:33 -0300

    feat: added 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "./components/layout";
-import { AddWorkout } from "./pages/add-workout";
-import { Home } from "./pages/home";
+import { Loading } from "./components/loading";
+import { NotFound } from "./pages/not-found";
 import { WorkoutDetails } from "./pages/workout-details";
+
+const Home = lazy(() =>
+  import("./pages/home").then((m) => ({ default: m.Home }))
+);
+
+const AddWorkout = lazy(() =>
+  import("./pages/add-workout").then((m) => ({ default: m.AddWorkout }))
+);
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/add" element={<AddWorkout />} />
-          <Route path="/workout/:id" element={<WorkoutDetails />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/add" element={<AddWorkout />} />
+            <Route path="/workout/:id" element={<WorkoutDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
